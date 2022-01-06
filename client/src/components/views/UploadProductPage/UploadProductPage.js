62c586743d1634d629d5781abbdcf930c221abc4
langram <langramjang@gmail.com>
2022-01-06 17:44:10 +0900

    인프런강의 - 1차커밋
    인프런강의 - 1차커밋

---
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function UploadProductPage() {
+    return (
+        <div>
+            테스트
+        </div>
+    )
+}
+
+export default UploadProductPage;
---
+++ server/config/dev.js
@@ -0,0 +1,3 @@
+module.exports = config = {
+    mongoURI:'mongodb+srv://reacom:5004@react-commerce.eqo90.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
